016af1b7109353bc24fd73b7f75568fe7d9d0bf6
Eliran Assaraf <eliran.a.bg@gmail.com>
2018-06-22 12:13:14 -0400

    text displayed fixed

---
+++ PhpProject1/testing.php
@@ -0,0 +1,12 @@
+<?php
+var_dump($_POST['list']);
+
+$myfile = fopen("testfile.txt", "a") or die ("Just die");
+$list = $_POST['list'];
+
+$str = $list . "\r\n";
+fwrite($myfile, $str);
+fclose($myfile);
+
+?>
+
